0770141e00cab4d41018501d344dc21e068bfdda
Lolli Di <290romchik@gmail.com>
2021-11-13 14:31:07 +0300

    интерфейс

--- CinemaDB/DobiRedFilm.xaml.cs
+++ CinemaDB/DobiRedFilm.xaml.cs
@@ -26,6 +26,8 @@ namespace CinemaDB
             InitializeComponent();
             CBOgran.ItemsSource = dbcl.dbP.Ограничения.Select(x => x.Возраст).ToList();
             CBZhanr.ItemsSource = dbcl.dbP.Жанры.Select(x => x.Жанр).ToList();
+            NazvFilm.ItemsSource = dbcl.dbP.Фильмы.Select(x => x.Название).ToList();
+            NazvFilm.Text = "";
         }
 
         private void DPDate_Changed(object sender, TextChangedEventArgs e)
@@ -37,6 +39,39 @@ namespace CinemaDB
             else
                 text=TBDate.Text.Substring(0,6);
             TBDate.Text = text + DPDate.Text;
+        }
+
+        private void TBCBChanged(object sender, TextChangedEventArgs e)
+        {
+            Фильмы p = dbcl.dbP.Фильмы.FirstOrDefault(x => x.Название == NazvFilm.Text);
+            if (p != null)
+            {
+                TBDate.IsEnabled = false;
+                DPDate.IsEnabled = false;
+                CBZhanr.IsEnabled = false;
+                CBOgran.IsEnabled = false;
+                TBDate.Text = p.Дата_выхода.ToString("HH:mm dd.MM.yyyy");
+                CBZhanr.Text = p.Жанры.Жанр;
+                CBOgran.Text = p.Ограничения.Возраст;
+            }
+            else
+            {
+                TBDate.IsEnabled = true;
+                DPDate.IsEnabled = true;
+                CBZhanr.IsEnabled = true;
+                CBOgran.IsEnabled = true;
+            }
+        }
+
+        private void CBChanged(object sender, TextChangedEventArgs e)
+        {
+            Сеансы p = dbcl.dbP.Сеансы.FirstOrDefault(x => x.Зал == Convert.ToInt32(CBZal.Text));
+            
+        }
+
+        private void SohrClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
