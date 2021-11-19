65167c8558ae776702d62eaf0d781fd1e9930cdb
Lolli Di <290romchik@gmail.com>
2021-11-19 08:28:07 +0300

    kab

---
+++ CinemaDB/Kabinet.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaDB
+{
+    /// <summary>
+    /// Логика взаимодействия для Kabinet.xaml
+    /// </summary>
+    public partial class Kabinet : Page
+    {
+        public Kabinet()
+        {
+            InitializeComponent();
+        }
+        private void BtnRedactClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
